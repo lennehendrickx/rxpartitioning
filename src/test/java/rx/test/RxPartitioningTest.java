a083dc473adddbdf4de6045ac842cbaf7e9c0f13
Lenne Hendrickx <lenne.h@sparkcentral.com>
2018-03-11 15:20:28 +0100

    Minor improvements

--- src/test/java/rx/test/RxPartitioningTest.java
+++ src/test/java/rx/test/RxPartitioningTest.java
@@ -14,101 +14,101 @@ public class RxPartitioningTest {
 
     private static final int MAX_NUMBER_OF_THREADS = 4;
 
-/**
- * Test fails.
- * Expected behavior: processing is evenly distributed accross 4 threads.
- * Example distribution for one test run: [801, 73, 71, 55]
- */
-@Test
-public void groupByWithParallel() {
+    /**
+     * Test fails.
+     * Expected behavior: processing is evenly distributed accross 4 threads.
+     * Example distribution for one test run: [801, 73, 71, 55]
+     */
+    @Test
+    public void groupByWithParallel() {
 
-    ConcurrentHashMap<String, Integer> threadProcessingCount = new ConcurrentHashMap<>();
+        ConcurrentHashMap<String, Integer> threadProcessingCount = new ConcurrentHashMap<>();
 
-    // GIVEN
-    Flowable<Integer> flowableWithGroupByAndParallel = Flowable.just(1, 2, 3, 4)
-            .repeat(250)
-            .groupBy(number -> number % MAX_NUMBER_OF_THREADS)
-            .parallel()
-            .runOn(Schedulers.io())
-            .flatMap(group -> group.doOnNext(number -> {
-                printThreadAndNumber(number);
-                increaseThreadProcessingCount(threadProcessingCount);
-            }))
-            .sequential();
+        // GIVEN
+        Flowable<Integer> flowableWithGroupByAndParallel = Flowable.just(1, 2, 3, 4)
+                .repeat(250)
+                .groupBy(number -> number % MAX_NUMBER_OF_THREADS)
+                .parallel()
+                .runOn(Schedulers.io())
+                .flatMap(group -> group.doOnNext(number -> {
+                    printThreadAndNumber(number);
+                    increaseThreadProcessingCount(threadProcessingCount);
+                }))
+                .sequential();
 
-    // WHEN
-    flowableWithGroupByAndParallel
-            .test()
-            .awaitTerminalEvent();
+        // WHEN
+        flowableWithGroupByAndParallel
+                .test()
+                .awaitTerminalEvent();
 
-    // THEN
-    assertThat(threadProcessingCount.values()).containsExactly(250, 250, 250, 250);
+        // THEN
+        assertThat(threadProcessingCount.values()).containsExactly(250, 250, 250, 250);
 
-}
+    }
 
-/**
- * Test fails.
- * Expected behavior: processing is evenly distributed accross 4 threads.
- * Example distribution for one test run: [801, 73, 71, 55]
- */
-@Test
-public void flatMapWithSubscribeOn() {
+    /**
+     * Test fails.
+     * Expected behavior: processing is evenly distributed accross 4 threads.
+     * Example distribution for one test run: [801, 73, 71, 55]
+     */
+    @Test
+    public void flatMapWithSubscribeOn() {
 
-    ConcurrentHashMap<String, Integer> threadProcessingCount = new ConcurrentHashMap<>();
+        ConcurrentHashMap<String, Integer> threadProcessingCount = new ConcurrentHashMap<>();
 
-    // GIVEN
-    Flowable<Integer> flowableWithFlapMapAndSubscribeOn = Flowable.just(1, 2, 3, 4)
-            .repeat(250)
-            .groupBy(number -> number % MAX_NUMBER_OF_THREADS)
-            .flatMap(group -> group.subscribeOn(Schedulers.io()).doOnNext(number -> {
-                printThreadAndNumber(number);
-                increaseThreadProcessingCount(threadProcessingCount);
-            }));
+        // GIVEN
+        Flowable<Integer> flowableWithFlapMapAndSubscribeOn = Flowable.just(1, 2, 3, 4)
+                .repeat(250)
+                .groupBy(number -> number % MAX_NUMBER_OF_THREADS)
+                .flatMap(group -> group.subscribeOn(Schedulers.io()).doOnNext(number -> {
+                    printThreadAndNumber(number);
+                    increaseThreadProcessingCount(threadProcessingCount);
+                }));
 
-    // WHEN
-    flowableWithFlapMapAndSubscribeOn
-            .test()
-            .awaitTerminalEvent();
+        // WHEN
+        flowableWithFlapMapAndSubscribeOn
+                .test()
+                .awaitTerminalEvent();
 
-    // THEN
-    assertThat(threadProcessingCount.values()).containsExactly(250, 250, 250, 250);
+        // THEN
+        assertThat(threadProcessingCount.values()).containsExactly(250, 250, 250, 250);
 
-}
+    }
 
-/**
- * Test succeeds.
- */
-@Test
-public void flatMapWithObserveOn() {
+    /**
+     * Test succeeds.
+     */
+    @Test
+    public void flatMapWithObserveOn() {
 
-    ConcurrentHashMap<String, Integer> threadProcessingCount = new ConcurrentHashMap<>();
+        ConcurrentHashMap<String, Integer> threadProcessingCount = new ConcurrentHashMap<>();
 
-    // GIVEN
-    Flowable<Integer> flowableWithFlapMapAndObserveOn = Flowable.just(1, 2, 3, 4)
-            .repeat(250)
-            .groupBy(number -> number % MAX_NUMBER_OF_THREADS)
-            .flatMap(group -> group.observeOn(Schedulers.io()).doOnNext(number -> {
-                printThreadAndNumber(number);
-                increaseThreadProcessingCount(threadProcessingCount);
-            }));
+        // GIVEN
+        Flowable<Integer> flowableWithFlapMapAndObserveOn = Flowable.just(1, 2, 3, 4)
+                .repeat(250)
+                .groupBy(number -> number % MAX_NUMBER_OF_THREADS)
+                .flatMap(group -> group.observeOn(Schedulers.io()).doOnNext(number -> {
+                    printThreadAndNumber(number);
+                    increaseThreadProcessingCount(threadProcessingCount);
+                }));
 
-    // WHEN
-    flowableWithFlapMapAndObserveOn
-            .test()
-            .awaitTerminalEvent();
+        // WHEN
+        flowableWithFlapMapAndObserveOn
+                .test()
+                .awaitTerminalEvent();
 
-    // THEN
-    assertThat(threadProcessingCount.values()).containsExactly(250, 250, 250, 250);
+        // THEN
+        assertThat(threadProcessingCount.values()).containsExactly(250, 250, 250, 250);
 
-}
+    }
 
-private static void printThreadAndNumber(Integer number) {
-    System.out.println(Thread.currentThread().getName() + " " + number);
-}
+    private static void printThreadAndNumber(Integer number) {
+        System.out.println(Thread.currentThread().getName() + " " + number);
+    }
 
-private static void increaseThreadProcessingCount(ConcurrentHashMap<String, Integer> threadCallCount) {
+    private static void increaseThreadProcessingCount(ConcurrentHashMap<String, Integer> threadCallCount) {
         threadCallCount
                 .compute(currentThread().getName(),
                         (thread, count) -> ofNullable(count).map(previousCount -> ++previousCount).orElse(1));
-}
+    }
 }
